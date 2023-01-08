284cf9d0f78243cc3d3056489ab02007e949e1e4
LastDevilHunter <62110196+LastDevilHunter@users.noreply.github.com>
2023-01-08 19:24:51 +0100

    Projektdateien hinzufügen.

---
+++ MathLib/MathLib.cpp
@@ -0,0 +1,13 @@
+﻿#include "MathLib.h"
+#include "Vector/Vector.h"
+
+using namespace std;
+
+int main()
+{
+	float test[] { 2, 4, 8 };
+	Vector<3> v1 = Vector<3>(test);
+	v1.print();
+	(v1 / 2).print();
+	std::cout << v1.length() << std::endl;
+}
---
+++ MathLib/Vector/Vector.h
@@ -0,0 +1,150 @@
+#pragma once
+#ifndef VECTOR
+#define VECTOR
+
+#include <iostream>
+
+template <int M> class Vector
+{
+private:
+    float values[M] {};
+public:
+    Vector() {};
+    template<float... Ts> requires (sizeof...(Ts) == M)
+        Vector(Ts &&... ts) : values{ts...}
+
+    Vector(const float* values) {
+        for (int i = 0; i < M; i++)
+        {
+            this->values[i] = values[i];
+        }
+    };
+
+    void print() const {
+        std::cout << std::fixed << this << ":{";
+        for (int i = 0; i < M; i++)
+        {
+            if (i != 0) std::cout << ", ";
+            std::cout << values[i];
+        }
+        std::cout << "}" << std::endl;
+    }
+
+    Vector crossProduct(Vector<3>& vector) requires (M == 3) {
+        return Vector<3> vec{};
+    }
+
+    float length() const {
+        return std::sqrt(squareLength());
+    }
+
+    float squareLength() const {
+        float sum {};
+        for (int i = 0; i < M; i++)
+        {
+            sum += values[i] * values[i];
+        }
+        return sum;
+    }
+
+    Vector<M> operator+(float vector) const {
+        Vector<M> vec{ *this };
+        vec += vector;
+        return vec;
+    }
+    Vector<M> operator+=(float vector) {
+        for (int i = 0; i < M; i++)
+        {
+            values[i] += vector;
+        }
+        return *this;
+    }
+    Vector<M> operator+(const Vector<M>& vector) const {
+        Vector<M> vec{ *this };
+        vec += vector;
+        return vec;
+    }
+    Vector<M>& operator+=(const Vector<M>& vector) {
+        for (int i = 0; i < M; i++) {
+            values[i] += vector[i];
+        }
+        return *this;
+    }
+
+    Vector<M> operator-(float vector) const {
+        Vector<M> vec{ *this };
+        vec -= vector;
+        return vec;
+    }
+    Vector<M> operator-=(float vector) {
+        for (int i = 0; i < M; i++)
+        {
+            values[i] -= vector;
+        }
+        return *this;
+    }
+    Vector<M> operator-(const Vector<M>& vector) const {
+        Vector<M> vec{ *this };
+        vec -= vector;
+        return vec;
+    }
+    Vector<M>& operator-=(const Vector<M>& vector) {
+        for (int i = 0; i < M; i++) {
+            values[i] -= vector[i];
+        }
+        return *this;
+    }
+
+    Vector<M> operator*(float vector) const {
+        Vector<M> vec{ *this };
+        vec *= vector;
+        return vec;
+    }
+    Vector<M> operator*=(float vector) {
+        for (int i = 0; i < M; i++)
+        {
+            values[i] *= vector;
+        }
+        return *this;
+    }
+    Vector<M> operator*(const Vector<M>& vector) const {
+        Vector<M> vec{ *this };
+        vec *= vector;
+        return vec;
+    }
+    Vector<M>& operator*=(const Vector<M>& vector) {
+        for (int i = 0; i < M; i++) {
+            values[i] *= vector[i];
+        }
+        return *this;
+    }
+
+    Vector<M> operator/(float vector) const {
+        Vector<M> vec{ *this };
+        vec /= vector;
+        return vec;
+    }
+    Vector<M> operator/=(float vector) {
+        for (int i = 0; i < M; i++)
+        {
+            values[i] /= vector;
+        }
+        return *this;
+    }
+    Vector<M> operator/(const Vector<M>& vector) const {
+        Vector<M> vec{ *this };
+        vec /= vector;
+        return vec;
+    }
+    Vector<M>& operator/=(const Vector<M>& vector) {
+        for (int i = 0; i < M; i++) {
+            values[i] /= vector[i];
+        }
+        return *this;
+    }
+
+    float operator[](int index) const {
+        return values[index];
+    }
+};
+#endif // !VECTOR
